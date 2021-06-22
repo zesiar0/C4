352d29f72b1fd2669de61de33d6eeff47d9d9907
zesiar0 <zeng921359373@163.com>
2021-06-22 23:28:52 +0800

    Initial commit

---
+++ src/main/java/com/c4demo/C4demoApplication.java
@@ -0,0 +1,13 @@
+package com.c4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C4demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c4demo/C4demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
